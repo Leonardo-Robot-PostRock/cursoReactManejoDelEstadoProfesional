c8c593a0bb439d4f9f6ad419d787884c9776570b
Leonardo Puebla <leonardo38361@gmail.com>
2022-02-08 17:24:04 -0300

    Maneras de actualizar un estado

--- src/UseState.js
+++ src/UseState.js
@@ -1,17 +1,28 @@
 import React from "react";
 
+const SECURITY_CODE = 'paradigma';
+
 function UseState({ name }) {
-    const [error, setError] = React.useState(true);
+    const [value, setValue] = React.useState('');
+    const [error, setError] = React.useState(false);
     const [loading, setLoading] = React.useState(false);
+
+    console.log(value)
 
     React.useEffect(() => {
         console.log("Empezando el efecto")
-
         if (!!loading) {
             setTimeout(() => {
                 console.log("Haciendo la validación")
-
+                if (value !== SECURITY_CODE) {
+                    setError(true)
+                }
+                else {
+                    setError(false)
+                }
                 setLoading(false);
+
+
 
                 console.log("terminando la validación")
             }, 3000);
@@ -23,16 +34,27 @@ function UseState({ name }) {
         <div>
             <h2>Eliminar UseState</h2>
             <p>Por favor escribe el p&aacute;rrafo de seguridad.</p>
-            {error && (
+            {(error && !loading) && (
                 <p>Error: el c&oacute;digo es incorrecto</p>
             )}
+
             {loading && (
                 <p>Cargando...</p>
             )}
 
-            <input placeholder="Código de seguridad" />
+            <input
+                placeholder="Código de seguridad"
+                value={value}
+                onChange={(event) => {
+                    // setError(false); otra solución
+                    setValue(event.target.value)
+                }}
+            />
             <button
-                onClick={() => setLoading(true)}
+                onClick={() => {
+                    // setError(false); Solución para quitar estado de error una vez haya sido mostrado
+                    setLoading(true);
+                }}
             >Comprobar</button>
         </div>
     );
